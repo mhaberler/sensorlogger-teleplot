5ab5a4ab8cb7d7b2a2cb8d0dfc6e2670c96a39b6
Michael Haberler <git@mah.priv.at>
2023-04-22 01:57:20 +0200

    proof-of-concept

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # teleplot relay for sensorlogger
 
+## step 1: run https://teleplot.fr/
+this will give you a UDP socket like `teleplot.fr:4711`
+
+## step 2: run the this like so:
+
+````
+(penv) BigM1:sensorlogger-teleplot mah$ TELEPLOT=teleplot.fr:11646 flask run --host=0.0.0.0 --port=8000
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on all addresses (0.0.0.0)
+ * Running on http://127.0.0.1:8000
+ * Running on http://172.16.0.104:8000
+Press CTRL+C to quit
+172.16.0.111 - - [22/Apr/2023 01:43:02] "POST /data HTTP/1.1" 200 -
+172.16.0.111 - - [22/Apr/2023 01:43:02] "POST /data HTTP/1.1" 200 -
+172.16.0.111 - - [22/Apr/2023 01:43:02] "POST /data HTTP/1.1" 200 -
+172.16.0.111 - - [22/Apr/2023 01:43:03] "POST /data HTTP/1.1" 200 -
+````
+
+## step 3: run sensorlogger with http push
+
+- configure URI as: `http://172.16.0.104:8000/data` 
+- hit `Tap to Test Pushing`
+- sensorlogger should display `Got status: 200`
+- hit `start recording``
+
+https://teleplot.fr should start displaying values:
+
+
+<img src="docs/teleplot.png"/>
+
---
+++ app.py
@@ -0,0 +1,45 @@
+
+# logs sensorlogger to relay to teleplot.fr
+
+# run as:
+# TELEPLOT=teleplot.fr:<destport> flask run --host=0.0.0.0 --port=<local portnumber>
+
+# in sensorlogger, configure HTTP push as:
+# http://<thisip>:<local portnumber>/data
+
+import json
+import os
+import socket
+from flask import Flask, request
+
+
+teleplot = os.environ["TELEPLOT"] 
+host,port=  teleplot.split(':')
+teleplotAddr = (host, int(port))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def teleplot(msg):
+     payload = msg["payload"]
+     for r in payload:
+          sensor = r["name"]
+          if sensor == "test":
+              print("got test push message")
+              continue
+          time = r["time"]/1000000
+          for name, value  in r["values"].items():
+            msg = f"{sensor}.{name}:{time}:{value}|np"
+            # print(msg)
+            sock.sendto(msg.encode(), teleplotAddr)
+
+                 
+app = Flask(__name__)
+
+@app.route("/data", methods=["POST"])
+def data():  # listens to the data streamed from the sensor logger
+    if str(request.method) == "POST":
+        data = json.loads(request.data)
+        teleplot(data)
+    return "success"
+
